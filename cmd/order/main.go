85e4640effef3f235879d4d51c97f8c56e73535b
Werner <inqast@gmail.com>
2022-05-31 18:11:26 +0300

    [feat] Дополнена сборка сервиса заказов

--- cmd/order/main.go
+++ cmd/order/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"github.com/Shopify/sarama"
+	orderHandler "github.com/inqast/saga-order/internal/order/handler"
 	"google.golang.org/grpc"
 	"log"
 	"net"
@@ -10,13 +12,17 @@ import (
 	"github.com/inqast/saga-order/internal/config"
 	"github.com/inqast/saga-order/internal/db"
 	"github.com/inqast/saga-order/internal/order/app"
+	cartClient "github.com/inqast/saga-order/internal/order/client/cart"
 	"github.com/inqast/saga-order/internal/order/repository"
+	pbcart "github.com/inqast/saga-order/pkg/api/cart"
 	pb "github.com/inqast/saga-order/pkg/api/order"
 )
 
 func main() {
 	ctx := context.Background()
 	cfg := config.GetConfigFromFile()
+	saramaCfg := sarama.NewConfig()
+	saramaCfg.Producer.Return.Successes = true
 
 	db.Migrate(&cfg.Order.Database)
 
@@ -25,7 +31,28 @@ func main() {
 		log.Fatal(err)
 	}
 
-	newServer := app.New(repository.New(adp))
+	conn, err := grpc.Dial(cfg.Cart.Grpc.Address(), grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	client := cartClient.New(pbcart.NewCartClient(conn))
+	repo := repository.New(adp)
+
+	producer, err := sarama.NewSyncProducer([]string{cfg.Kafka.Brokers}, saramaCfg)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	handler, err := orderHandler.New(saramaCfg, repo)
+	handler.StartConsuming(ctx, cfg.Kafka)
+
+	newServer, err := app.New(repo, client, producer)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
 	lis, err := net.Listen("tcp", cfg.Order.Grpc.Address())
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
